bcb4d0ef2e6a1dd5b5ebba4276eebd95c77a214a
AlphaKretin <deewhydeeecks@gmail.com>
2025-05-23 17:51:06 +0930

    Convert to Hundred Line sorter

---
+++ src/js/data/2025-05-23.js
@@ -0,0 +1,87 @@
+dataSetVersion = "2025-05-23"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [];
+
+dataSet[dataSetVersion].characterData = [
+    {
+        name: "Takumi Sumino",
+        img: "takumi.png",
+        opts: {},
+    },
+    {
+        name: "Takemaru Yakushiji",
+        img: "takemaru.png",
+        opts: {},
+    },
+    {
+        name: "Hiruko Shizuhara",
+        img: "hiruko.png",
+        opts: {},
+    },
+    {
+        name: "Darumi Amemiya",
+        img: "darumi.png",
+        opts: {},
+    },
+    {
+        name: "Eito Aotsuki",
+        img: "eito.png",
+        opts: {},
+    },
+    {
+        name: "Tsubasa Kawana",
+        img: "tsubasa.png",
+        opts: {},
+    },
+    {
+        name: "Gaku Maruko",
+        img: "gaku.png",
+        opts: {},
+    },
+    {
+        name: "Ima Tsukumo",
+        img: "ima.png",
+        opts: {},
+    },
+    {
+        name: "Kako Tsukumo",
+        img: "kako.png",
+        opts: {},
+    },
+    {
+        name: "Shouma Ginzaki",
+        img: "shouma.png",
+        opts: {},
+    },
+    {
+        name: "Nozomi Kirifuji",
+        img: "nozomi.png",
+        opts: {},
+    },
+    {
+        name: "Kurara Oosuzuki",
+        img: "kurara.png",
+        opts: {},
+    },
+    {
+        name: "Kyoshika Magadori",
+        img: "kyoshika.png",
+        opts: {},
+    },
+    {
+        name: "Yugamu Omokage",
+        img: "yugamu.png",
+        opts: {},
+    },
+    {
+        name: "Moko Mojiro",
+        img: "moko.png",
+        opts: {},
+    },
+    {
+        name: "Eva",
+        img: "eva.png",
+        opts: {},
+    },
+];
